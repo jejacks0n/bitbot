5e5cd6ca43c6d6535ce596b22dbbdcc3f7272d7c
jejacks0n <jejacks0n@gmail.com>
2015-02-19 19:21:35 -0700

    allow an array of tokens.

--- lib/bitbot/listener/web.rb
+++ lib/bitbot/listener/web.rb
@@ -57,8 +57,16 @@ module Bitbot
         return false unless req.post? && req.path == @path                     # only posts at the desired path
         return false unless req['text'].is_a?(String)                          # no empty messages
         return false unless req['user_name'] != Bitbot.configuration.user_name # no replies to myself
-        return false unless @token == req['token']                             # no messages not from slack
+        return false unless valid_token?(req['token'])                         # no messages not from slack
         req
+      end
+
+      def valid_token?(token)
+        if @token.is_a?(Array)
+          @token.include?(token)
+        else
+          @token == token
+        end
       end
 
     end
